0ffc55e64f6d5c384686d806d6e97a5aaa643a07
Talha Aftab <talhaaftab98@gmail.com>
2022-03-29 22:51:56 +0500

    Code-Refactor

--- JWT_WebApi/Controllers/UserController.cs
+++ JWT_WebApi/Controllers/UserController.cs
@@ -9,84 +9,11 @@ namespace JWT.WebApi.Web.Controllers
     [Route("api/[controller]"), ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IAuthManager authManager;
         private readonly IUserManager userManager;
 
-
-        public UserController(IAuthManager authManager, IUserManager userManager)
+        public UserController(IUserManager userManager)
         {
-            this.authManager = authManager;
             this.userManager = userManager;
-
-        }
-
-        [HttpPost, Route("register")]
-        public async Task<ApiResponse<User>?> RegisterAsync(UserDto request)
-        {
-            var apiResponse = new ApiResponse<User>();
-            if (request == null)
-                return new ApiResponse<User>()
-                {
-                    Content = null,
-                    Message = "Information is empty"
-                };
-            var user = new User()
-            {
-                UserName = request.UserName,
-                EmailAddress = request.EmailAddress
-            };
-            var hasUser = await userManager.CheckUserAsync(user);
-            if (hasUser is null)
-            {
-                var response = await userManager.RegisterUserAsync(user, request.Password);
-                return response;
-            }
-            else
-            {
-                apiResponse.Content = null;
-                apiResponse.Message = "User already exist";
-                apiResponse.Status = System.Net.HttpStatusCode.OK;
-            }
-            return apiResponse;
-        }
-
-        [HttpPost, Route("login")]
-        public async Task<ApiResponse<string>> LoginAsync(UserDto request)
-        {
-            var apiResponse = new ApiResponse<string>();
-            if (request == null)
-                return new ApiResponse<string>()
-                {
-                    Content = null,
-                    Message = "Information is empty"
-                };
-            var user = new User()
-            {
-                UserName = request.UserName,
-                EmailAddress = request.EmailAddress
-            };
-
-            var hasUser = await userManager.CheckUserAsync(user);
-            if (hasUser is not null)
-            {
-                var isVerified = await authManager.VerifyPasswordAsync(hasUser, request.Password);
-                if (!isVerified)
-                {
-                    apiResponse.Message = "Wrong password";
-                    apiResponse.Status = System.Net.HttpStatusCode.BadRequest;
-                    return apiResponse;
-                }
-                var token = authManager.GenerateTokenAsync(hasUser);
-                apiResponse.Content = token;
-                apiResponse.Message = "User can login with token";
-                apiResponse.Status = System.Net.HttpStatusCode.OK;
-            }
-            else
-            {
-                apiResponse.Message = "User not found, make sure you're registered.";
-                apiResponse.Status = System.Net.HttpStatusCode.BadRequest;
-            }
-            return apiResponse;
         }
 
         [HttpGet, Authorize(Roles = "Admin")]
@@ -95,13 +22,5 @@ namespace JWT.WebApi.Web.Controllers
             var result = await this.userManager.GetAllUserAsync();
             return Ok(result);
         }
-
-        [HttpGet, Route("ClaimName"), Authorize]
-        public IActionResult GetClaimName()
-        {
-            var result = this.userManager.GetClaimName();
-            return Ok(result);
-        }
-
     }
 }
